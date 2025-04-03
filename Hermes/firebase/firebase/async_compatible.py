741e64a6bc847c284ef35b389b31272d34f891fc
Jeff Burke <jburke@remap.ucla.edu>
2025-04-02 18:28:04 -0700

    Update async_compatible.py to avoid issue with rapid-fire first cues.

--- Hermes/firebase/firebase/async_compatible.py
+++ Hermes/firebase/firebase/async_compatible.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import threading
 
 from .lazy import LazyLoadProxy
 
@@ -6,12 +7,13 @@ from .lazy import LazyLoadProxy
 __all__ = ['process_pool']
 
 _process_pool = None
-
+_global_lock = threading.Lock()
 
 def get_process_pool(size=5):
     global _process_pool
-    if _process_pool is None:
-        _process_pool = multiprocessing.Pool(processes=size)
+    with _global_lock:
+        if _process_pool is None:
+            _process_pool = multiprocessing.Pool(processes=size)
     return _process_pool
 
 
